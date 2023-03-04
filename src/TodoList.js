007b85f87fdcf326a45f33d86af9a0111ef0ab2c
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-03-04 10:40:16 +0530

    markup & feat: added todoList markup and features

--- src/TodoList.js
+++ src/TodoList.js
@@ -13,7 +13,6 @@ function TodoList() {
   // Update task useStates
   const [show, setShow] = useState(false);
   const [updateProdId, setUpdateProdId] = useState("");
-  
 
   const navigate = useNavigate();
 
@@ -25,20 +24,14 @@ function TodoList() {
       .then((res) => res.json())
       .then((data) => {
         setTodoList(data.todoList);
-        data.todoList.forEach((task) => {
-          
-        });
       })
       .catch((err) => console.log(err.message));
   }, []);
-
-  
 
   function addTask(e) {
     e.preventDefault();
     let token = JSON.parse(localStorage.getItem("token"));
     let addTaskURL = "http://localhost:4000/api/v1/user/todolist/addtask";
-    
 
     fetch(addTaskURL, {
       method: "POST",
@@ -57,9 +50,10 @@ function TodoList() {
     })
       .then((res) => res.json())
       .then((data) => {
+        console.log(data)
         setTodoList(data.updatedList);
       })
-      .catch((err) => (console.log(err.message)));
+      .catch((err) => console.log(err.message));
   }
 
   function handleLogout() {
@@ -73,60 +67,90 @@ function TodoList() {
       .catch((err) => console.log(err.message));
   }
 
-  
   return (
-    <div >
-      <nav>
+    <div>
+      <nav className={styles.navbar}>
+        <h1>TO DO LIST</h1>
         <button onClick={handleLogout}>Logout!</button>
       </nav>
-      <h1>TO DO LIST</h1>
-      <ul className={styles.todolist}>
-        {todoList &&
-          todoList.map((task) => {
-            return (
-              <li key={task._id} className={styles.task} >
-                <div onClick = {() => {
-                navigate('/todolist/item' , {state : {taskId : task._id}})
-              }}>{task.name}</div>
-                
-                <DeleteButton taskId = {task._id} setTodoList = {setTodoList}/>
-                <button
-                  onClick={() => {
-                    setShow((value) => !value);
-                    setUpdateProdId(task._id);
-                  }}
-                >
-                  Update Task
-                </button>
-              </li>
-            );
-          })}
-      </ul>
+      <div className={styles["main-container"]}>
+        <div className={styles["list-container"]}>
+          <ul className={styles.todolist}>
+            {todoList &&
+              todoList.map((task) => {
+                return (
+                  <li key={task._id} className={styles.task}>
+                    <div
+                      onClick={() => {
+                        navigate("/todolist/item", {
+                          state: { taskId: task._id },
+                        });
+                      }}
+                      className={styles["task-name"]}
+                    >
+                      {task.name}
+                    </div>
+                    <div className={styles["task-button-div"]}>
+                      <DeleteButton
+                        taskId={task._id}
+                        setTodoList={setTodoList}
+                      />
+                      <button
+                        onClick={() => {
+                          setShow((value) => !value);
+                          setUpdateProdId(task._id);
+                        }}
+                        className={styles["update-task-btn"]}
+                      >
+                        Update Task
+                      </button>
+                    </div>
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
 
-      <div>
-        <form onSubmit={addTask}>
-          <label>
-            Name of Task:
-            <input onChange={(e) => setTaskName(e.target.value)} type="text" />
-          </label>
+        <div className={styles["addtask-form-container"]}>
+          <h2>ADD TASK</h2>
+          <form onSubmit={addTask}>
+            <label>
+              Name of Task:
+              <input
+                onChange={(e) => setTaskName(e.target.value)}
+                type="text"
+              />
+            </label>
 
-          <label>
-            {/* expectedCompletionDate */}
-            Date of completion:
-            <input onChange={(e) => setTaskDate(e.target.value)} type="date" />
-          </label>
+            <label>
+              
+              Date of completion:
+              <input
+                onChange={(e) => setTaskDate(e.target.value)}
+                type="date"
+              />
+            </label>
 
-          <label>
-            description
-            <input onChange={(e) => setTaskDesc(e.target.value)} type="text" />
-          </label>
+            <label>
+              Description:
+              <input
+                onChange={(e) => setTaskDesc(e.target.value)}
+                type="text"
+              />
+            </label>
 
-          <button>Submit</button>
-        </form>
+            <button className={styles["add-task-submit-btn"]}>Submit</button>
+          </form>
+        </div>
       </div>
 
       {show && (
-        <UpdateForm updateProdId={updateProdId} setTodoList = {setTodoList}/>
+        <UpdateForm
+          updateProdId={updateProdId}
+          setTodoList={setTodoList}
+          open={show}
+          isClosed={() => setShow(false)}
+        />
       )}
     </div>
   );
