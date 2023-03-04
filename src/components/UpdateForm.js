f39c17f084747871481958ba33977f978d8a993c
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-03-04 10:41:08 +0530

    feat : added update task feature

--- src/components/UpdateForm.js
+++ src/components/UpdateForm.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import styles from "./updateForm.module.css";
 
-function UpdateForm({setTodoList , updateProdId}) {
+function UpdateForm({ setTodoList, updateProdId, open, isClosed }) {
   const [updatedName, setUpdatedName] = useState("");
   const [updatedDate, setUpdatedDate] = useState("");
   const [updatedDesc, setUpdatedDesc] = useState("");
   const [updatedStatus, setUpdatedStatus] = useState();
+
   function updateTask(e) {
     e.preventDefault();
     let token = JSON.parse(localStorage.getItem("token"));
@@ -27,44 +29,58 @@ function UpdateForm({setTodoList , updateProdId}) {
     })
       .then((res) => res.json())
       .then((data) => {
-        if(setTodoList){
-            setTodoList(data.updatedTodoList)
+        if (setTodoList) {
+          setTodoList(data.updatedTodoList);
         }
       })
       .catch((err) => console.log(err.message));
   }
+
   return (
-    <div>
-      <form onSubmit={updateTask}>
-        <label>
-          Enter Name:
-          <input type="text" onChange={(e) => setUpdatedName(e.target.value)} />
-        </label>
+    <div className={styles["main-container"]}>
+      <div className={styles["update-form"]}>
+        <button className={styles['close-btn']} onClick={isClosed}> X </button>
+        <form onSubmit={updateTask}>
+          <label>
+            <div>Enter Name:</div>
 
-        <label>
-          Enter Date:
-          <input type="date" onChange={(e) => setUpdatedDate(e.target.value)} />
-        </label>
+            <input
+              type="text"
+              onChange={(e) => setUpdatedName(e.target.value)}
+            />
+          </label>
 
-        <label>
-          Enter Description:
-          <input type="text" onChange={(e) => setUpdatedDesc(e.target.value)} />
-        </label>
+          <label>
+            <div>Enter Date:</div>
+            <input
+              type="date"
+              onChange={(e) => setUpdatedDate(e.target.value)}
+            />
+          </label>
 
-        <label>
-          Enter Status
-          <select
-            value={updatedStatus}
-            onChange={(e) => setUpdatedStatus(e.target.value)}
-          >
-            <option value="pending">pending</option>
-            <option value="completed">completed</option>
-            <option value="overdue">overdue</option>
-          </select>
-        </label>
+          <label>
+            <div>Enter Description:</div>
+            <input
+              type="text"
+              onChange={(e) => setUpdatedDesc(e.target.value)}
+            />
+          </label>
 
-        <input type="submit" value="Submit" />
-      </form>
+          <label>
+            Enter Status
+            <select
+              value={updatedStatus}
+              onChange={(e) => setUpdatedStatus(e.target.value)}
+            >
+              <option value="pending">pending</option>
+              <option value="completed">completed</option>
+              <option value="overdue">overdue</option>
+            </select>
+          </label>
+
+          <button className={styles["submit-btn"]}>SUBMIT</button>
+        </form>
+      </div>
     </div>
   );
 }
