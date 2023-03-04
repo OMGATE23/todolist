c778ee00969aa9280a7a15ad4efbbca5a1b6b357
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-03-04 10:40:46 +0530

    feat : added delete task feature

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,33 +1,34 @@
 import { useNavigate } from "react-router-dom";
+import styles from './deleteButton.module.css'
 
-function DeleteButton({taskId , setTodoList}){
+function DeleteButton({ taskId, setTodoList }) {
+  const navigate = useNavigate();
+  function deleteTask(taskId) {
+    let token = JSON.parse(localStorage.getItem("token"));
+    let deleteURL =
+      "http://localhost:4000/api/v1/user/todolist/deletetask/" + taskId;
 
-  const navigate = useNavigate()
-    function deleteTask(taskId) {
-        let token = JSON.parse(localStorage.getItem("token"));
-        let deleteURL =
-          "http://localhost:4000/api/v1/user/todolist/deletetask/" + taskId;
-    
-        fetch(deleteURL, {
-          method: "DELETE",
-          headers: { Authorization: `Bearer ${token}` },
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            if(setTodoList){
-                setTodoList(data.updatedTodoList)
-            }
+    fetch(deleteURL, {
+      method: "DELETE",
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (setTodoList) {
+          setTodoList(data.updatedTodoList);
+        }
 
-            if(!setTodoList){
-              navigate('/todolist')
-            }
-          }).catch(err => console.log(err.message))
-
-        
-      }
-    return (
-        <button onClick={() => deleteTask(taskId)}>Delete Task</button>
-    )
+        if (!setTodoList) {
+          navigate("/todolist");
+        }
+      })
+      .catch((err) => console.log(err.message));
+  }
+  return (
+    <button className= {styles['delete-btn']} onClick={() => deleteTask(taskId)}>
+      <i className="material-icons" >delete</i>
+    </button>
+  );
 }
 
-export default DeleteButton
+export default DeleteButton;
