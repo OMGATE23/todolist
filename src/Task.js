b24b48211da684ffebbbad5ff260e89d4828d0d1
omgate <90276220+OMGATE23@users.noreply.github.com>
2023-03-04 10:39:50 +0530

    markup & feat : added task markup and features

--- src/Task.js
+++ src/Task.js
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DeleteButton from "./components/DeleteButton";
 import UpdateForm from "./components/UpdateForm";
+import styles from "./task.module.css";
 
 function Task() {
   const location = useLocation();
   const [task, setTask] = useState();
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     let token = JSON.parse(localStorage.getItem("token"));
@@ -17,30 +19,51 @@ function Task() {
     )
       .then((res) => res.json())
       .then((data) => {
-        setTask(data)
+        setTask(data);
       })
       .catch((err) => err.message);
   }, [location.state.taskId]);
 
   useEffect(() => {
-    console.log(task)
-  } , [task])
+    console.log(task);
+  }, [task]);
 
   return (
-    <div>
-      The individual task
-      {location.state.taskId}
-      {task && <div>
-        <div>Name : {task.name} </div>
-        <div>Description : {task.description}</div>
-        <div>Status : {task.status}</div>
-        <UpdateForm updateProdId = {task._id} />
-        <DeleteButton taskId = {task._id} />
-      </div>}
+    <div className={styles["main-container"]}>
+      <div className={styles["task-container"]}>
+        
+        {task && (
+          <div className={styles['task']}>
+            <div><span className={styles['task-title']}> Name : </span>{task.name} </div>
+            <div><span className={styles['task-title']}>Task ID : </span>{location.state.taskId}</div>
+            <div><span className={styles['task-title']}>Description : </span>{task.description}</div>
+            <div><span className={styles['task-title']}>Status : </span>{task.status}</div></div>)}
 
-      
-      
-      
+            { task && ( 
+            <div className={styles['button-container']}>
+              <DeleteButton taskId={task._id} />
+              <button
+                onClick={() => {
+                  setShow((value) => !value);
+                }}
+                className={styles["update-btn"]}
+              >
+                Update Task
+              </button>
+
+              
+              {show && (
+                <UpdateForm
+                  updateProdId={task._id}
+                  open={show}
+                  isClosed={() => setShow(false)}
+                />
+              )}
+              
+            </div>
+          
+        )}
+      </div>
     </div>
   );
 }
